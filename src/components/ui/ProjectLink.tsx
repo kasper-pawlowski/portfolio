077a7deac8d66be4874d13f095b66d1330e2e098
kasper-pawlowski <kapi28000@gmail.com>
2025-07-20 14:28:27 +0200

    projects cursor

--- src/components/ui/ProjectLink.tsx
+++ src/components/ui/ProjectLink.tsx
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 import React from 'react'
 import projectsData from '@/data/projects.json'
-import { ArrowUpRight } from 'lucide-react'
-import GithubLogo from '../../../public/icons/github.svg'
+import { Cursor } from '../core/cursor'
+import { MousePointer2 } from 'lucide-react'
 
 type ProjectLinkProps = {
   id: number
@@ -10,7 +10,7 @@ type ProjectLinkProps = {
 
 const ProjectLink = ({ id }: ProjectLinkProps) => {
   return (
-    <Link href={`/project/${id}`} className='z-5 h-full w-full'>
+    <Link href={`/project/${id}`} className='relative z-5 h-full w-full'>
       <video
         controls={false}
         autoPlay
@@ -23,6 +23,34 @@ const ProjectLink = ({ id }: ProjectLinkProps) => {
         <source src={projectsData[id - 1].videoUrl} type='video/webm' />
         Twój browser nie obsługuje tagu wideo.
       </video>
+      <Cursor
+        attachToParent
+        variants={{
+          initial: { scale: 0.3, opacity: 0 },
+          animate: { scale: 1, opacity: 1 },
+          exit: { scale: 0.3, opacity: 0 }
+        }}
+        transition={{
+          ease: 'easeInOut',
+          duration: 0.15
+        }}
+        className='top-2 left-2'
+      >
+        <div className='cursor-parent relative'>
+          <MousePointer2
+            style={{ color: projectsData[id - 1].accent }}
+            className='h-6 w-6'
+          />
+          <div
+            className='cursor-child absolute ml-4 rounded-lg px-2 text-neutral-50'
+            style={{ backgroundColor: projectsData[id - 1].accent }}
+          >
+            <span className='font-display font-500 text-2xl whitespace-nowrap'>
+              {projectsData[id - 1].nameKey}
+            </span>
+          </div>
+        </div>
+      </Cursor>
     </Link>
   )
 }
