0b2eaffb892102a0ce03e5d277659f94e74a8877
kasper-pawlowski <kapi28000@gmail.com>
2025-08-08 00:56:37 +0200

    volume

--- src/hooks/useSound.ts
+++ src/hooks/useSound.ts
@@ -3,8 +3,10 @@ import { playSound } from 'react-sounds'
 const useSound = () => {
   const soundButton = () => playSound('/sounds/button.wav')
   const soundTap = () => playSound('/sounds/tap.wav')
-  const soundTransitionDown = () => playSound('/sounds/transition_down.wav')
-  const soundTransitionUp = () => playSound('/sounds/transition_up.wav')
+  const soundTransitionDown = () =>
+    playSound('/sounds/transition_down.wav', { volume: 0.4 })
+  const soundTransitionUp = () =>
+    playSound('/sounds/transition_up.wav', { volume: 0.4 })
   const soundHover = () => playSound('/sounds/hover.wav')
   const soundClick = () => playSound('/sounds/click.wav')
   const soundPlum = () => playSound('/sounds/plum.wav')
