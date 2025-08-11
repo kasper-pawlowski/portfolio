496547acd167caa71a4ee24b5ddcbf7fe38aa277
kasper-pawlowski <kapi28000@gmail.com>
2025-08-11 02:27:58 +0200

    add vercel analitics and speedinsights

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,6 +3,9 @@ import { Suspense } from 'react'
 import { NextIntlClientProvider, hasLocale } from 'next-intl'
 import { notFound } from 'next/navigation'
 import { ReactLenis } from 'lenis/react'
+import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next'
+import { getMessages, getTranslations } from 'next-intl/server'
 
 import Loading from '@/app/[locale]/Loading'
 import { routing } from '@/i18n/routing'
@@ -13,7 +16,6 @@ import StyledToaster from '@/components/ui/StyledToaster'
 import { LoaderProvider } from '@/context/LoaderContext'
 import { goia_display, goia } from '../fonts'
 import '../globals.css'
-import { getMessages, getTranslations } from 'next-intl/server'
 
 export async function generateMetadata({
   params
@@ -100,6 +102,8 @@ export default async function LocaleLayout({
             </Providers>
           </NextIntlClientProvider>
         </Suspense>
+        <Analytics />
+        <SpeedInsights />
         <script
           type='application/ld+json'
           dangerouslySetInnerHTML={{
